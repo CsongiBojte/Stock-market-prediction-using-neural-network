de1d7e178d0956fee0032c4b0c3ff6b582341b55
CsongiBojte <bojte.csongi12@gmail.com>
2025-02-04 18:16:19 +0000

    WIP: Normalize data with simple MinMaxScaler and build a simple well organized LSTM model

--- config.py
+++ config.py
@@ -6,10 +6,17 @@ BASE_PATH = os.path.join(ROOT_PATH, 'models/lstm')
 RAW_DATA_PATH = os.path.join(BASE_PATH, 'data/raw')
 PROCESSED_DATA_PATH = os.path.join(BASE_PATH, 'data/processed')
 CHECKPOINTS_PATH = os.path.join(BASE_PATH, 'models/checkpoints')
+TRAINING_PATH = os.path.join(BASE_PATH,'models/training')
 RESULTS_PATH = os.path.join(BASE_PATH, 'results')
 FIGURES_PATH = os.path.join(RESULTS_PATH, 'figures')
 METRICS_PATH = os.path.join(RESULTS_PATH, 'metrics')
 NOTEBOOKS_PATH = os.path.join(BASE_PATH, 'notebooks')
+
+SCALERS_PATH = os.path.join(BASE_PATH, 'models/scalers')
+TRAIN_PATH = os.path.join(PROCESSED_DATA_PATH, 'train')
+VAL_PATH = os.path.join(PROCESSED_DATA_PATH, 'val')
+TEST_PATH = os.path.join(PROCESSED_DATA_PATH, 'test')
+
 
 WINDOW_SIZE = 14
 BATCH_SIZE = 15
